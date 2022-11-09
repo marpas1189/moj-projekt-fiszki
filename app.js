70ee792d8d576ccb651263e35fa3a74673889f61
Adam Piskorek <adam.piskorek.pl@gmail.com>
2022-11-09 21:57:33 +0100

    Local Storage

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
+// - Wyswietlic na kartkach flashcardsach
+// - Bierzemy wartoci uzytkownika
+// - Zebrac i dodac do obiektu
+// - Zdobyc aktualna liste slow
+// - Ten obiekt musimy dodac do listy slow
+// - Ta liste zapisac na localStorage
+
+setFlashcardValues(getFromStorage());
+
+// function showAvailableFlashcards() {
+//   let flashcard = localStorage.getItem('word');
+//   flashcard = JSON.parse(flashcard);
+//   console.log('Z localStorage: ', flashcard.word, flashcard.definition);
+// }
+
+function setFlashcardValues(flashcard) {
+  document.getElementById('word1').innerHTML = flashcard.word;
+  document.getElementById('def1').innerHTML = flashcard.definition;
+}
+
+function addToLocalStorage() {
+  // - Bierzemy wartoci uzytkownika
+  let flashcard = getFlashcardValues();
+  // - zapisac na localStorage
+
+  let wordsList = [];
+  wordsList.push(flashcard);
+
+  localStorage.setItem('wordsList', JSON.stringify(wordsList));
+}
+
 function getFlashcardValues() {
   const word = document.getElementById('add-word').value;
   const definition = document.getElementById('add-definition').value;
-
-  console.log(word);
-  console.log(definition);
-
   // - zebrac i dodac do obiektu
-  let obj = {
+  return {
     word: word,
     definition: definition,
   };
-
-  console.log(obj);
-
-  return obj;
 }
 
-function storeInLocalStorage() {
-  // - Bierzemy wartoci uzytkownika
-  let flashcard = getFlashcardValues();
+function getFromStorage() {
+  let flashcard = localStorage.getItem('wordsList');
+  flashcardsList = JSON.parse(flashcard);
 
-  // - zapisac na localStorage
-  localStorage.setItem('word', JSON.stringify(flashcard));
+  console.log(flashcardsList[0].word, flashcardsList[0].definition);
+  return flashcardsList;
 }
-
-function showStorage() {
-  let flashcard = localStorage.getItem('word');
-  flashcard = JSON.parse(flashcard);
-
-  console.log(flashcard.word, flashcard.definition);
-}
-
-// - Wyswietlic na kartkach flashcardsach
